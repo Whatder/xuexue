9aa3f55f28fff05616599de22d4e4bbe280b6f01
何何何 <523035216@qq.com>
2018-03-05 02:21:14 +0800

    底部导航栏

--- app/src/main/java/com/nkbh/xuexue/MainActivity.java
+++ app/src/main/java/com/nkbh/xuexue/MainActivity.java
@@ -1,14 +1,32 @@
 package com.nkbh.xuexue;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.bottomNav)
+    BottomNavigationBar bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initBottomNav();
     }
-    //commit test
+
+    private void initBottomNav() {
+        bottomNav.addItem(new BottomNavigationItem(R.drawable.ic_launcher, "计划")).setActiveColor(R.color.colorPrimary)
+                .addItem(new BottomNavigationItem(R.drawable.ic_launcher, "学习")).setActiveColor(R.color.colorPrimary)
+                .addItem(new BottomNavigationItem(R.drawable.ic_launcher, "交流")).setActiveColor(R.color.colorPrimary)
+                .initialise();
+    }
 }
