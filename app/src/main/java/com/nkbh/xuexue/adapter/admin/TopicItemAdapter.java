93e6daec640425cd7191ed3b961da9278322880d
何何何 <523035216@qq.com>
2018-04-03 17:08:52 +0800

    主题管理列表 显示

---
+++ app/src/main/java/com/nkbh/xuexue/adapter/admin/TopicItemAdapter.java
@@ -0,0 +1,67 @@
+package com.nkbh.xuexue.adapter.admin;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.nkbh.xuexue.R;
+import com.nkbh.xuexue.bean.TopicBean;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+/**
+ * Created by User on 2018/4/3.
+ */
+
+public class TopicItemAdapter extends RecyclerView.Adapter<TopicItemAdapter.ViewHolder> {
+    private Context mContext;
+    private List<TopicBean> data;
+
+    public TopicItemAdapter(Context mContext, List<TopicBean> data) {
+        this.mContext = mContext;
+        this.data = data;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(mContext).inflate(R.layout.item_admin_topic, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Glide.with(mContext).load(data.get(position).getProfile_pic()).into(holder.ivPic);
+        holder.tvAuthor.setText(data.get(position).getName());
+        holder.tvContent.setText(data.get(position).getContent());
+    }
+
+    @Override
+    public int getItemCount() {
+        return data.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.ivPic)
+        CircleImageView ivPic;
+        @BindView(R.id.tvAuthor)
+        TextView tvAuthor;
+        @BindView(R.id.tvContent)
+        TextView tvContent;
+        @BindView(R.id.tvDel)
+        TextView tvDel;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
+}
--- app/src/main/java/com/nkbh/xuexue/fragment/AdminTopicManagerFragment.java
+++ app/src/main/java/com/nkbh/xuexue/fragment/AdminTopicManagerFragment.java
@@ -1,13 +1,41 @@
 package com.nkbh.xuexue.fragment;
 
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
 import com.nkbh.xuexue.R;
+import com.nkbh.xuexue.adapter.admin.TopicItemAdapter;
 import com.nkbh.xuexue.base.BaseFragment;
+import com.nkbh.xuexue.bean.ResponseBean;
+import com.nkbh.xuexue.bean.TopicBean;
+import com.nkbh.xuexue.network.RetrofitHelper;
+import com.nkbh.xuexue.network.ServiceApi;
+import com.nkbh.xuexue.utils.ToastUtils;
+import com.scwang.smartrefresh.layout.SmartRefreshLayout;
+import com.scwang.smartrefresh.layout.api.RefreshLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by User on 2018/4/3.
  */
 
 public class AdminTopicManagerFragment extends BaseFragment {
+    @BindView(R.id.rvManager)
+    RecyclerView rvManager;
+    @BindView(R.id.refreshLayout)
+    SmartRefreshLayout refreshLayout;
+
+    TopicItemAdapter adapter;
+    List<TopicBean> data = new ArrayList<>();
+
     @Override
     protected int getLayoutID() {
         return R.layout.fragment_admin_common_fragment;
@@ -15,6 +43,47 @@ public class AdminTopicManagerFragment extends BaseFragment {
 
     @Override
     protected void initParameter() {
+        adapter = new TopicItemAdapter(mActivity, data);
+        rvManager.setLayoutManager(new LinearLayoutManager(mActivity));
+        rvManager.setAdapter(adapter);
+        getData(null);
+    }
+
+    private void getData(final RefreshLayout refreshLayout) {
+        ServiceApi service = RetrofitHelper.getService();
+        service.getTopic()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<ResponseBean<List<TopicBean>>>() {
+                    @Override
+                    public void onSubscribe(Disposable d) {
+
+                    }
+
+                    @Override
+                    public void onNext(ResponseBean<List<TopicBean>> value) {
+                        if ("succ".equals(value.getStatus())) {
+                            data.clear();
+                            data.addAll(value.getData());
+                            adapter.notifyDataSetChanged();
+                        } else
+                            ToastUtils.show(mActivity, value.getMsg());
+                        if (refreshLayout != null)
+                            refreshLayout.finishRefresh();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        ToastUtils.show(mActivity, e.getMessage());
+                        if (refreshLayout != null)
+                            refreshLayout.finishRefresh();
+                    }
+
+                    @Override
+                    public void onComplete() {
+
+                    }
+                });
 
     }
 }
